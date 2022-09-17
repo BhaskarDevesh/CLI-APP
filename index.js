3192c9129ad59a43672a7fb1d98380fce90e4b8e
BhaskarDevesh <bhaskardevesh@gmail.com>
2022-09-17 15:17:20 +0530

    added last part of quiz
    added last part of quiz

--- index.js
+++ index.js
@@ -29,3 +29,19 @@ var questions = [{ question: "What is my title?\n a:Bhaskar\n b:Kumar\n c:Singh\
 { question: "Who is my favorite MCU character?\n a:Thor\n b:Ironman\n c:Spiderman\n", answer: "b" },
 { question: "What is my favorite color?\n a:Red\n b:Blue\n c:Black\n", answer: "b" },
 { question: "Where is my hometown?\n a:Odisha\n b:Delhi\n c:Bihar\n", answer: "c" }]
+
+
+for (var i = 0; i < questions.length; i++) {
+    currentQ = questions[i];
+    quiz(currentQ.question, currentQ.answer);
+  }
+  
+  console.log("Yay your final score is: ", score);
+  console.log("High score: ");
+  
+  for (var i = 0; i < highscore.length; i++) {
+    console.log(highscore[i].name, highscore[i].score);
+  }
+  
+  console.log("Send me screenshot if you scored more than high score\n");
+  console.log("_____________________________________________");
