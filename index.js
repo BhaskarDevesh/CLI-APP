3f77e25b0c1bbe9065b30f83d4feacd0366b7919
BhaskarDevesh <bhaskardevesh@gmail.com>
2022-09-17 15:11:06 +0530

    created function

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ console.log("Welcome " + userName + " to do you know me QUIZ? ");
 console.log("Choose the right option.");
 console.log();
 score = 0;
+
+var highscore = [{ name: "Ajju", score: 10 }];
+
+
+function quiz(question, answer) {
+  userAnswer = readlineSync.question(question);
+  if (userAnswer === answer) {
+    console.log("Right! ");
+    score = score + 4;
+  } else {
+    console.log("Wrong! ");
+    score--;
+  }
+  console.log("Your score is: ", score);
+  console.log("---------------------------------------------\n");
+}
