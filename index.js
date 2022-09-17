8607e45f8974aa7ff4b666653f7026a00d48bc53
BhaskarDevesh <bhaskardevesh@gmail.com>
2022-09-17 15:14:54 +0530

    added questions

--- index.js
+++ index.js
@@ -23,3 +23,9 @@ function quiz(question, answer) {
   console.log("Your score is: ", score);
   console.log("---------------------------------------------\n");
 }
+
+var questions = [{ question: "What is my title?\n a:Bhaskar\n b:Kumar\n c:Singh\n", answer: "a" },
+{ question: "What is my age?\n a:20\n b:21\n c:22\n", answer: "a" },
+{ question: "Who is my favorite MCU character?\n a:Thor\n b:Ironman\n c:Spiderman\n", answer: "b" },
+{ question: "What is my favorite color?\n a:Red\n b:Blue\n c:Black\n", answer: "b" },
+{ question: "Where is my hometown?\n a:Odisha\n b:Delhi\n c:Bihar\n", answer: "c" }]
